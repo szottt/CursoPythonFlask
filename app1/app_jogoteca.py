327a4913e1a7bc842ee62f125485de36717e432e
szottt <szottt@hotmail.com>
2020-06-18 22:31:07 -0300

    Inclui novas rotas

--- app1/app_jogoteca.py
+++ app1/app_jogoteca.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request , redirect, session, flash, url_for
+from flask import Flask, render_template, request , redirect, session, flash, url_for, send_from_directory
 from dao import JogoDao,UsuarioDao
 from models import Jogo, Usuario
 import pymysql
+import os
 
 app = Flask(__name__)
 app.secret_key = 'alura'
@@ -11,6 +12,7 @@ MYSQL_USER      = "root"
 MYSQL_PASSWORD  = "todobancogosta@zika1391"
 MYSQL_DB        = "jogoteca"
 MYSQL_PORT      = "3306"
+UPLOAD_PATH     = os.path.dirname(os.path.abspath(__file__)) + "\\upload"
 
 db = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD,db=MYSQL_DB)
 
@@ -36,7 +38,11 @@ def criar():
     categoria = request. form['categoria']
     console = request. form['console']
     jogo = Jogo(nome, categoria, console)
-    jogo_dao.salvar(jogo)
+    jogo = jogo_dao.salvar(jogo)
+
+    arquivo = request.files['arquivo']
+    upload_path = UPLOAD_PATH
+    arquivo.save(f'{upload_path}\\capa{jogo.id}.jpg')
     return redirect(url_for('index'))
 
 @app.route('/editar/<int:id>')
@@ -44,7 +50,7 @@ def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar')))
     jogo = jogo_dao.busca_por_id(id)
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo = f'capa{id}.jpg')
 
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
@@ -87,5 +93,8 @@ def logout():
     flash('Nenhum usuário logado!')
     return redirect(url_for('index'))
 
+@app.route('/upload/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('upload', nome_arquivo)
 
 app.run(debug=True)
---
+++ app1/teste.py
@@ -0,0 +1,6 @@
+import os
+
+UPLOAD_PATH     = os.path.dirname(os.path.abspath(__file__)) + r"\upload"
+UPLOAD_PATH2    = r"C:\Users\Igor Szot\Documents\Cursos2020\CursoPythonFlask\app1\upload"
+
+print(UPLOAD_PATH2)
